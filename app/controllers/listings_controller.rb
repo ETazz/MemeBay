e8d75631ff38edcb783b5cc7bb3066f4ab1de39c
ETazz <GCAM022112@coderacademy.edu.au>
2022-03-18 15:39:23 +1100

    fixing errors, adding in active storage, messing with some styling

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,13 +1,67 @@
 class ListingsController < ApplicationController
+  before_action :authenticate_memeber!, except: [:index, :show]
+  before_action :set_listing, only: [:show, :edit, :update, :destroy]
+  before_action :set_form_vars, only: [:new, :edit]
   def index
     @listings = Listing.all
   end
 
   def category
-    @listing = Listing.find(Category)
+    
   end
 
   def show
-    @listing = Listing.find(params[:id])
+
+    
+  end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = current_memeber.listings.new(listing_params)
+    if @listing.save 
+      redirect_to @listing, notice: "Listing succesfully created"
+    else
+      pp @listing.errors
+      set_form_vars
+      render "new", notice: "Something went wrong"
   end
 end
+
+  def edit
+
+  end
+
+  def update
+    @Listing.update(listing_params)
+    if @listing.save 
+      redirect_to @listing, notice: "Listing succesfully updated"
+    else
+      pp @listing.errors
+      set_form_vars
+      render "edit", notice: "Something went wrong"
+  end
+  end
+
+  def destroy
+    @listing.destroy
+  end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(:title, :price, :category_id, :description)
+  end
+
+  def set_listing
+    @listing = Listing.find(params[:id])
+  end
+
+  def set_form_vars
+    @categories = Category.all
+  end
+
+end
+
--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,7 +1,6 @@
 module ListingsHelper
 
-
-    def format_price(price)
-        "#{price/ 100}( ͡° ͜ʖ ͡°)"
-    end
+def format_price(price)
+    "#{price/ 100}( ͡° ͜ʖ ͡°)"   
+end    
 end
